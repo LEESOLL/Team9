bfebad928a85aa9572737af44d0aa5e98d6eb175
Lee Sol <leeso4567@naver.com>
2023-01-19 21:52:32 +0900

    판매자 권한 요청의 상태를 구분해주는 Enum 클래스 생성

--- src/main/java/com/example/market9/entity/PermissionStatusEnum.java
+++ src/main/java/com/example/market9/entity/PermissionStatusEnum.java
@@ -2,20 +2,20 @@ package com.example.market9.entity;
 
 public enum PermissionStatusEnum {
 
-    ACCEPT(Authority.ACCEPT),  // 수락
-    WAITING(Authority.WAITING),  // 대기
-    REFUSE(Authority.REFUSE); // 거절
-    private final String authority;
+    ACCEPT(PermissionAuthority.ACCEPT),  // 수락
+    WAITING(PermissionAuthority.WAITING),  // 대기
+    REFUSE(PermissionAuthority.REFUSE); // 거절
+    private final String permissionAuthority;
 
-    AuthorityStatusEnum(String authority) {
-        this.authority = authority;
+    PermissionStatusEnum(String authority) {
+        this.permissionAuthority = authority;
     }
 
     public String getAuthority() {
-        return this.authority;
+        return this.permissionAuthority;
     }
 
-    public static class Authority {     ///시큐리티에서 ... 구분할려고..! 스트링값을 읽음... 11
+    public static class PermissionAuthority {     ///시큐리티에서 ... 구분할려고..! 스트링값을 읽음... 11
         public static final String ACCEPT = "AUTH_ACCEPT";
         public static final String WAITING = "AUTH_WAITING";
         public static final String REFUSE = "AUTH_REFUSE";
