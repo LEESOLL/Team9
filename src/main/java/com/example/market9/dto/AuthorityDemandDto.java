0a7977c9ea2228a75f632cc541ed8852bd07c22a
Lee Sol <leeso4567@naver.com>
2023-01-19 21:50:07 +0900

    판매자 권한 요청 객체를 보내는 DTO 구현

--- src/main/java/com/example/market9/dto/AuthorityDemandDto.java
+++ src/main/java/com/example/market9/dto/AuthorityDemandDto.java
@@ -1,21 +1,22 @@
 package com.example.market9.dto;
 
-import com.example.market9.entity.AuthorityRequest;
+import com.example.market9.entity.AuthorityDemand;
 import com.example.market9.entity.PermissionStatusEnum;
 import lombok.Getter;
 
 @Getter
-public class AuthorityRequestDto {
+public class AuthorityDemandDto {
 
     private String username;
     private String category;
     private String introduce;
     private PermissionStatusEnum role;
 
-    public AuthorityRequestDto(AuthorityRequest authorityRequest) {
-        this.username = authorityRequest.getUsername();;
-        this.category = authorityRequest.getCategory();
-        this.introduce = authorityRequest.getIntroduce();
-        this.role = authorityRequest.getRole();
+    public AuthorityDemandDto(AuthorityDemand authorityDemand) {
+        this.username = authorityDemand.getUsername();;
+        this.category = authorityDemand.getCategory();
+        this.introduce = authorityDemand.getIntroduce();
+        this.role = authorityDemand.getRole();
     }
+
 }
