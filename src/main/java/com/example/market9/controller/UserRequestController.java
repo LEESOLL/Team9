87f47dc18743e61b6dfaae8ea8902f1b49294e24
J-Jaeh <1wo2gur@naver.com>
2023-01-20 17:43:12 +0900

    [refactor] 정렬기준 및 정렬방법 추가 및 디폴트값 저장

--- src/main/java/com/example/market9/controller/UserRequestController.java
+++ src/main/java/com/example/market9/controller/UserRequestController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -59,13 +60,18 @@ public class UserRequestController {
      * @return PurchaseRequestRepository 에서  유저네임으로 조회 되는 값을 반환
      */
     @GetMapping("/request")  //전체요청보기 ..,
-    public RequestSellerListResponseDto getRequestAllSellerList(@RequestBody OnlyUserNameDto userName ,
-                                                                @RequestParam(value = "page",required = false,defaultValue ="1") Integer page,
-                                                                @RequestParam(value = "size",required = false,defaultValue = "2") Integer size
-                                                                ){  //시큐리티 적용시 바뀔사항...유저네임꺼내기 !
+    public RequestSellerListResponseDto getRequestAllSellerList(
+            @RequestBody OnlyUserNameDto userName ,
+            @RequestParam(value = "page",required = false,defaultValue ="1") Integer page,
+            @RequestParam(value = "size",required = false,defaultValue = "2") Integer size,
+            @RequestParam(value = "isAsc",required = false,defaultValue = "true")Boolean isAsc,
+            @RequestParam(value = "sortBy",required = false,defaultValue = "createdAt")String sortBy)
+    {
+        //시큐리티 적용시 바뀔사항...유저네임꺼내기 !
 
-
-        Pageable pageRequest = PageRequest.of(page-1,size);
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC:Sort.Direction.DESC;
+        Sort sort = Sort.by(direction,sortBy);
+        Pageable pageRequest = PageRequest.of(page-1,size,sort);
 
         return  requestService.getRequestAllSellerList(userName.getUserName(), pageRequest);
     }
